7fb7658cfe7603aa38173c904c34e0a8c6d6ac68
SkinnyFatMan <SkinnyFatMan@gmail.com>
2015-09-16 15:14:49 -1000

    finished tests up till equip

--- test/test-script.js
+++ test/test-script.js
@@ -40,14 +40,18 @@ describe('Food', function() {
 });
 
 describe('Player', function() {
+  var bob;
 
-  var bob = new Player('bob', 4, 3, 1);
+  beforeEach(function () {
+    bob = new Player('bob', 4, 3, 1);
+  });
 
   it('should be a class', function() {
     expect(Player).to.be.a('function');
   });
   it('should have a name', function() {
     expect(bob).to.have.property('name');
+    expect(bob.name).to.equal('bob');
   });
   it('should have health', function() {
     expect(bob).to.have.property('health');
@@ -63,6 +67,74 @@ describe('Player', function() {
   });
   it('should be unequipped', function() {
     expect(bob).to.have.property('equipped').equal(false);
+  });
+  it('should not be an asshole', function () {
+    expect(bob).to.not.have.property('asshole');
+  });
+
+  describe('getPack', function() {
+    it('should be a function returning an array', function() {
+      expect(bob.getPack).to.be.a('function');
+      expect(bob.getPack()).to.deep.equal([]);
+    });
+  });
+  describe('getMaxHealth', function() {
+    it('should be a function returning max player HP', function() {
+      expect(bob.getMaxHealth).to.be.a('function');
+      expect(bob.getMaxHealth()).to.equal(4);
+    });
+  });
+
+  describe('checkPack', function() {
+    it('should be a function', function() {
+      expect(bob.checkPack).to.be.a('function');
+
+      //cannot perform other tests due to sinon requirement for console log check
+    });
+  });
+  describe('takeItem', function() {
+    it('should be a function', function() {
+      expect(bob.takeItem).to.be.a('function');
+    });
+    it('should not allow more than three items in bag', function() {
+      var pocky = new Food('pocky', 2304857892347);
+      bob.takeItem(pocky);
+      bob.takeItem(pocky);
+      bob.takeItem(pocky);
+      expect(bob.takeItem(pocky)).to.equal(false);
+    });
+  });
+  describe('discardItem', function() {
+    it('should be a function', function() {
+      expect(bob.discardItem).to.be.a('function');
+    });
+    it('should remove an item from the bag', function() {
+      var pocky = new Food('pocky', 2304857892347);
+      bob.takeItem(pocky);
+      bob.takeItem(pocky);
+      bob.discardItem(pocky);
+      expect(bob.getPack()).to.be.length(1);
+    });
+  });
+  describe('equip', function() {
+    it('should be a function', function() {
+      expect(bob.equip).to.be.a('function');
+    });
+  });
+  describe('eat', function() {
+    it('should be a function', function() {
+      expect(bob.eat).to.be.a('function');
+    });
+  });
+  describe('useItem', function() {
+    it('should be a function', function() {
+      expect(bob.useItem).to.be.a('function');
+    });
+  });
+  describe('equippedWith', function() {
+    it('should be a function', function() {
+      expect(bob.equippedWith).to.be.a('function');
+    });
   });
 });
 
